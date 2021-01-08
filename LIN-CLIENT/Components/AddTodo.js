66669da61be63406710dbe7d692fd3944c57ace3
Xenia <lommy101@gmail.com>
2021-01-08 22:49:31 +0900

    Add input text and button form done..

--- LIN-CLIENT/Components/AddTodo.js
+++ LIN-CLIENT/Components/AddTodo.js
@@ -1,45 +1,67 @@
 import * as React from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { 
     TextInput,
     View,
-    Button,
+    Text,
+    TouchableHighlight,
     StyleSheet,
     Dimensions ,
-    KeyboardAvoidingView 
+    KeyboardAvoidingView,
 } from 'react-native';
 
 const behavior = Platform.OS === "ios" ? "padding" : null
-const keyboardVerticalOffset = Platform.OS === 'ios' ? 64 : 0
-
+const keyboardVerticalOffset = Platform.OS === 'ios' ? 86 : 0
 var {width, height} = Dimensions.get('window');
 
 const AddTodo = () => {
     return (
         <KeyboardAvoidingView behavior={behavior} keyboardVerticalOffset={keyboardVerticalOffset}>
             <View style={styles.inputArea}>
-                <TextInput                    
-                    style={styles.input}
-                    placeholder="Add ToDo"
-                    placeholderTextColor={'#999'}
-                    autoCorrect={false}
-                    underlineColorAndroid='transparent'
-                />
-                <Button title="Submit" />
+                <View style={{flex:5}}>
+                    <TextInput                    
+                        style={styles.input}
+                        placeholder=""
+                        placeholderTextColor={'#999'}
+                        autoCorrect={false}
+                        underlineColorAndroid='transparent'
+                    />
+                </View>
+                <View style={{flex:2}}>
+                    <TouchableHighlight style={styles.button}>
+                        <MaterialCommunityIcons style={styles.submitText}
+                            name="arrow-up-drop-circle"
+                            color="#A593E0"
+                        />
+                    </TouchableHighlight>
+                </View>
             </View>
         </KeyboardAvoidingView>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#A593E0',
+    inputArea: {
         width: width,
-        flexDirection: 'row',
-        height: 10
+        flexDirection:"row",
+        padding: 4,
+        backgroundColor: '#fff',
     },
     input: {
-        width: width-20,
-        backgroundColor: '#A593E0',
+        borderRadius: 10,
+        textAlign: 'left',
+        alignSelf: 'flex-start',
+        backgroundColor: '#ececec',
+        width: width-60,
+        fontSize: 24,
+    },
+    button: {
+        paddingTop: 2,
+        marginRight: 15,
+        alignSelf: 'flex-end',
+        textAlign: 'center',
+    },
+    submitText: {
         fontSize: 24,
     }
 })
